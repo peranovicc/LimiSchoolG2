ee9837dc880b8f862fceed3e1513f44e9713542f
peranovicc <cvijanperanovic@aol.com>
2020-09-11 18:15:50 +0200

    Објекти - решење задатка

--- 4. Nedelja/objekti-zadatak.js
+++ 4. Nedelja/objekti-zadatak.js
@@ -17,3 +17,20 @@ let recept = {
     sastojci: [s1,s2]
 }
 
+let ukupnaCena = 0
+
+for(let i = 0; i < recept.sastojci.length; i++){
+    let trenutniSastojak = recept.sastojci[i]
+    ukupnaCena += trenutniSastojak.cena * trenutniSastojak.kolicina
+}
+
+console.log(ukupnaCena)
+
+
+let x = [s1,s2]
+for(let i = 0; i < x.length; i++){
+    let trenutniSastojak = x[i]
+    ukupnaCena += trenutniSastojak.cena * trenutniSastojak.kolicina
+}
+
+
