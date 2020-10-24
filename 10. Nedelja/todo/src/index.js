53c505b485545bf20fc913ab2601813421254b1e
peranovicc <cvijanperanovic@aol.com>
2020-10-24 15:43:17 +0200

    react, usestate

--- 10. Nedelja/todo/src/index.js
+++ 10. Nedelja/todo/src/index.js
@@ -1,24 +1,42 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+const Statistics = ({za,protiv,uzdrzani}) => {
+  let ukupno = za + protiv + uzdrzani
 
-  const [brojac,setBrojac] = useState(0)
-  
+  if(ukupno !== 0){
+    return (
+      <>
+        <p>+ : {za}</p>
+        <p>- : {protiv}</p>
+        <p>0 : {uzdrzani}</p>
+        <p>Ukupno: {ukupno}</p>
+        <p>Procenat pozitivnih: {za / ukupno * 100}%</p>
+      </>
+    )
+  }
+  else{
+    return (
+      <p>Nema ocena</p>
+    )
+  }
+}
+
+const App = () => {
+  const [pozitivno,setPozitivno] = useState(0)
+  const [negativno,setNegativno] = useState(0)
+  const [neutralno,setNeutralno] = useState(0)
   return (
     <>
-      <p>{brojac}</p>
-      <button 
-        onClick={
-          () => setBrojac(brojac + 1)
-        }
-      >
-        x
-      </button>
-      <p>{brojac}</p>
-      <p>{brojac}</p>
-      <p>{brojac}</p>
-
+      <button onClick={() => setPozitivno(pozitivno + 1)}>+</button>
+      <button onClick={() => setNegativno(negativno + 1)}>-</button>
+      <button onClick={() => setNeutralno(neutralno + 1)}>0</button>
+      {/* <p>+ : {pozitivno}</p>
+      <p>- : {negativno}</p>
+      <p>0 : {neutralno}</p>
+      <p>Ukupno: {ukupno}</p>
+      <p>Procenat pozitivnih: {pozitivno / ukupno * 100}%</p> */}
+      <Statistics za={pozitivno} protiv={negativno} uzdrzani={neutralno} />
     </>
   )
 }
